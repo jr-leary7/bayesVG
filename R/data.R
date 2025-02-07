dd602f17e739be741119f428ee0f5d399b36c1d2
Jack Leary <j.leary@ufl.edu>
2025-02-06 19:01:01 -0500

    added data to the package -- closes #8

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' A \code{Seurat} object containing spatial data.
+#'
+#' This file contains the anterior mouse brain data from 10X Genomics. 
+#'
+#' @format An object of class \code{Seurat}.
+#' @source https://github.com/satijalab/seurat-data
+#' @usage data(seu_brain)
+"seu_brain"
+
+#' A \code{Seurat} object containing scRNA-seq data.
+#'
+#' This file contains the pbmc3k data from 10X Genomics. 
+#'
+#' @format An object of class \code{Seurat}.
+#' @source https://github.com/satijalab/seurat-data
+#' @usage data(seu_pbmc)
+"seu_pbmc"
