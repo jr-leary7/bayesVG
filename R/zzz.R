233396d37e90a4b2a68873e47762a4b6997e002a
Jack Leary <jrleary@live.unc.edu>
2025-03-13 15:10:22 -0400

    updated .onLoad() in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(...) {
   cmdstan_version <- cmdstanr::cmdstan_version(error_on_NA = FALSE)
   if (is.null(cmdstan_version)) {
-    stop("No CmdStan installation found. Run cmdstanr::install_cmdstan() to install.", call. = FALSE)
+    cli::cli_abort("No CmdStan installation found. Run cmdstanr::install_cmdstan() to install.")
   }
 }
