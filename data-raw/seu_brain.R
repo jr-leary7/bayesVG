1334024b2a17e9e5b3d0197b841dfeaf4a93e9a1
Jack Leary <j.leary@ufl.edu>
2025-05-22 21:31:56 -0500

    updated spatial brain dataset to have better filtering

--- data-raw/seu_brain.R
+++ data-raw/seu_brain.R
@@ -1,7 +1,7 @@
 library(Seurat)
 seu_brain <- SeuratData::LoadData("stxBrain", type = "anterior1")
 gene_set_1 <- Matrix::rowSums(seu_brain@assays$Spatial$counts > 0) >= 10L
-gene_set_2 <- Matrix::rowMeans(seu_brain@assays$Spatial$counts) >= 0.1
+gene_set_2 <- Matrix::rowMeans(seu_brain@assays$Spatial$counts) >= 0.25
 genes_keep <- rownames(seu_brain)[gene_set_1 & gene_set_2]
 seu_brain <- subset(seu_brain, features = genes_keep)
 spot_set_1 <- seu_brain$nCount_Spatial >= 500L
@@ -12,4 +12,4 @@ seu_brain <- PercentageFeatureSet(seu_brain,
                                   pattern = "^mt-", 
                                   col.name = "percent_mito")
 seu_brain <- subset(seu_brain, subset = percent_mito < 20)
-save(seu_brain, file = "../data/seu_brain.rda", compress = "xz")
+save(seu_brain, file = "data/seu_brain.rda", compress = "xz")
